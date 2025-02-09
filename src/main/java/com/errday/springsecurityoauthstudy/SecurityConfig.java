7cdaec860080cd348ff597f6b62ff86a1e0c980e
javython999 <infinity2544@gmail.com>
2025-02-09 20:54:25 +0900

    httpBasic

--- src/main/java/com/errday/springsecurityoauthstudy/SecurityConfig.java
+++ src/main/java/com/errday/springsecurityoauthstudy/SecurityConfig.java
@@ -5,15 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-            .formLogin(withDefaults());
+            //.httpBasic(withDefaults())
+            .httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
+        ;
         //new CustomSecurityConfigurer().setFlag(true).configure(http);
         return http.build();
     }
